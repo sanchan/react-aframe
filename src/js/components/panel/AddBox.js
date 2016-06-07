e872ac6965ed08329b6e168fe0b94547d7e29b49
sanchan <danilo.dev@gmail.com>
2016-06-06 23:30:26 -0300

    Improved design

--- src/js/components/panel/AddBox.js
+++ src/js/components/panel/AddBox.js
@@ -1,27 +1,39 @@
 import React from "react";
 
-import MeshAttrsContainer from '../../containers/panel/MeshAttrsContainer';
+import MeshAttrs from './MeshAttrs';
 
 export default class AddBox extends React.Component {
   static defaults = {
-
+    depth: 1,
+    height: 1,
+    width: 1
   }
 
   render() {
-    const actions = {
-      onChangeColor: this.props.onChangeColor,
-      onChangeMetalness: this.props.onChangeMetalness,
-      onChangeOpacity: this.props.onChangeOpacity,
-      onChangeRoughness: this.props.onChangeRoughness,
-      onChangeShader: this.props.onChangeShader,
-      onChangeSrc: this.props.onChangeSrc,
-      onChangeTranslate: this.props.onChangeTranslate,
-      onChangeTransparent: this.props.onChangeTransparent
-    };
-
     return (
       <div>
-        <MeshAttrsContainer {...actions}></MeshAttrsContainer>
+        <MeshAttrs {...this.props.meshActions}></MeshAttrs>
+        <div class="row">
+          <h3>Box atrributes</h3>
+        </div>
+        <div class="row">
+          <label>Depth</label>
+          <div class="form-value">
+            <input type="text" placeholder={AddBox.defaults.depth} value={this.props.depth}  onChange={this.props.onChangeDepth} />
+          </div>
+        </div>
+        <div class="row">
+          <label>Height</label>
+          <div class="form-value">
+            <input type="text" placeholder={AddBox.defaults.height} value={this.props.height}  onChange={this.props.onChangeHeight} />
+          </div>
+        </div>
+        <div class="row">
+          <label>Width</label>
+          <div class="form-value">
+            <input type="text" placeholder={AddBox.defaults.width} value={this.props.width}  onChange={this.props.onChangeWidth} />
+          </div>
+        </div>
         <div class="row">
           <button class="submit" onClick={this.props.onSubmit}>Add Box</button>
         </div>
--- src/js/containers/panel/AddBoxContainer.js
+++ src/js/containers/panel/AddBoxContainer.js
@@ -8,9 +8,23 @@ export default class AddBoxContainer extends React.Component {
      this.setState(AddBox.defaults);
   }
 
-  // Actions
+  // Ok... this is tricky... lets explain it :)
+  // attr is the attribute we can set
+  // this returns a function that accepts an event that is going to be given by an input
+  // and this function is binded to this, so we are setting the state of this AddBoxContainer
+  // So, setting the attr value we are indicating "which attribute" to set, and with the e
+  // (given by an input) we saying "which value" to set.
+  // onChangeAttr(attr) {
+  //   return (function(e){
+  //     let attrs = {};
+  //     attrs[attr] = e.target.value;
+  //     this.setState(attrs);
+  //   }).bind(this)
+  // }
+
+  // Mesh Actions
   onChangeColor(e){
-    console.log("change color");
+    console.log("color");
     this.setState({color: e.target.value});
   }
 
@@ -42,12 +56,26 @@ export default class AddBoxContainer extends React.Component {
     this.setState({transparent: e.target.value});
   }
 
+  // Box Actions
+  onChangeDepth(e){
+    this.setState({depth: e.target.value});
+  }
+
+  onChangeHeight(e){
+    this.setState({height: e.target.value});
+  }
+
+  onChangeWidth(e){
+    this.setState({width: e.target.value});
+  }
+
+  // Submit Action
   onSubmit() {
     this.props.addBox(this.state);
   }
 
   render() {
-    const actions = {
+    const meshActions = {
       onChangeColor: ::this.onChangeColor,
       onChangeMetalness: ::this.onChangeMetalness,
       onChangeOpacity: ::this.onChangeOpacity,
@@ -55,12 +83,17 @@ export default class AddBoxContainer extends React.Component {
       onChangeShader: ::this.onChangeShader,
       onChangeSrc: ::this.onChangeSrc,
       onChangeTranslate: ::this.onChangeTranslate,
-      onChangeTransparent: ::this.onChangeTransparent,
-      onSubmit: ::this.onSubmit
+      onChangeTransparent: ::this.onChangeTransparent
+    };
+
+    const boxActions = {
+      onChangeDepth: ::this.onChangeDepth,
+      onChangeHeight: ::this.onChangeHeight,
+      onChangeWidth: ::this.onChangeWidth
     };
 
     return (
-      <AddBox {...actions}></AddBox>
+      <AddBox meshActions={meshActions} {...boxActions} onSubmit={::this.onSubmit}></AddBox>
     )
   }
 }
